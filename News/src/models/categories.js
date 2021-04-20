35e3f1f7087df6007c0bbb7201b5c35869c6b737
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2021-04-20 09:25:16 +0900

    [ADD] News 과제

---
+++ News/src/models/categories.js
@@ -0,0 +1,31 @@
+export const categories = [
+  {
+    name: '대구',
+    path: '?category=대구',
+  },
+
+  {
+    name: '서울',
+    path: '?category=서울',
+  },
+
+  {
+    name: '부산',
+    path: '?category=부산',
+  },
+
+  {
+    name: '경기도',
+    path: '?category=경기도',
+  },
+
+  {
+    name: '전주',
+    path: '?category=전주',
+  },
+
+  {
+    name: '강원도',
+    path: '?category=강원도',
+  },
+];
